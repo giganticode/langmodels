763dd599d4fea915283d6dae012115aa3f769a2f
Hlib <hlibbabii@gmail.com>
2019-08-06 09:00:13 +0200

    fix unit-tests accorfing to last changes

--- tests/config/serialization.py
+++ tests/config/serialization.py
@@ -4,7 +4,7 @@ import dataprep.api.corpus as api
 import jsons
 
 from langmodels.config.datamodel import LMTrainingConfig, LstmArch, TrainingProcedure, LearningRateCycle, \
-    PrepFunction
+    PrepFunction, Corpus
 from langmodels.config.serialization import dump_config, load_config_from_string
 
 
@@ -12,13 +12,14 @@ class DatamodelTest(unittest.TestCase):
     def test_serialize_deserialize_defaults(self):
         lm_training_config = LMTrainingConfig(
             base_model=None,
-            dataset='/home/hlib/dev/yahtzee',
+            corpus=Corpus(path='/home/hlib/dev/yahtzee',
+                            extensions="java"),
             prep_function=PrepFunction(
                 api.bpe,
                 ['10k'],
                 {'no_str': True, 'no_com': True, 'no_case': True, 'no_spaces': False, 'no_unicode': False}
             ),
-            arch=LstmArch(),
+            arch=LstmArch(), bs=32, bptt=200,
             training_procedure=TrainingProcedure(base_lr=1e-3,
                                                  cycle=LearningRateCycle(n=1, len=1,
                                                                          mult=1)))

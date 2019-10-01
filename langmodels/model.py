736fd77ec16219f05041ecce86834b697cc2bbc4
hlibbabii <hlibbabii@gmail.com>
2019-10-01 17:44:41 +0200

    add better models

--- langmodels/model.py
+++ langmodels/model.py
@@ -26,6 +26,8 @@ MODEL_ZOO_PATH_ENV_VAR = 'MODEL_ZOO_PATH'
 RANDOM_MODEL_NAME = 'dev_10k_1_10_190923.132328'
 TINY_MODEL_NAME = 'langmodel-small-split_10k_1_512_190906.154943'
 SMALL_MODEL_NAME = 'langmodel-medium-split_10k_1_512_190924.154542_-_langmodel-medium-split_10k_1_512_190925.141033'
+SMALL_MODEL_1K = 'langmodel-medium-split_1k_1_512_190926.101149'
+MEDIUM_MODEL = 'langmodel-large-split_10k_1_512_190926.120146'
 
 PAD_TOKEN_INDEX = 1
 
@@ -145,7 +147,7 @@ class TrainedModel(object):
 
     @classmethod
     def get_default_model(cls, force_use_cpu: bool = False) -> 'TrainedModel':
-        return TrainedModel.get_small_model(force_use_cpu)
+        return TrainedModel.get_medium_model(force_use_cpu)
 
     @classmethod
     def get_random_model(cls, force_use_cpu: bool = False) -> 'TrainedModel':
@@ -158,6 +160,10 @@ class TrainedModel(object):
     @classmethod
     def get_small_model(cls, force_use_cpu: bool = False) -> 'TrainedModel':
         return TrainedModel.load_model_by_name(SMALL_MODEL_NAME, force_use_cpu)
+
+    @classmethod
+    def get_medium_model(cls, force_use_cpu: bool = False) -> 'TrainedModel':
+        return TrainedModel.load_model_by_name(MEDIUM_MODEL_NAME, force_use_cpu)
 
     @classmethod
     def load_model_by_name(cls, name: str, force_use_cpu: bool = False) -> 'TrainedModel':

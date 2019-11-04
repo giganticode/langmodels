3477ca13ad842412e9fa00fb5d73dfa2da1f773b
hlib <hlibbabii@gmail.com>
2019-11-04 14:49:09 +0200

    loading a trained model for testing from disk rather than from the internet

--- tests/test_beamsearch.py
+++ tests/test_beamsearch.py
@@ -1,10 +1,13 @@
+import os
+
 import torch
 
-from langmodels.modelregistry import load_model_with_tag
+from langmodels import project_dir
+from langmodels.modelregistry import load_from_path
 
 
 def test_hidden_state_not_changed_after_beam_search():
-    trained_model = load_model_with_tag('DEFAULT')
+    trained_model = load_from_path(os.path.join(project_dir, 'data/models/dev_10k_1_10_190923.132328'))
     trained_model.feed_text("Hi there, it's")
     before = trained_model.model[0].hidden
     trained_model.predict_next_full_token(n_suggestions=10)

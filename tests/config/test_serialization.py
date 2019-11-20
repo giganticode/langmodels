86e4e1ffdcbdd2ff1ed97b1778f824170e70e4a6
hlib <hlibbabii@gmail.com>
2019-11-20 15:59:21 +0200

    fix unit-tests after serialization issue fix

--- tests/config/test_serialization.py
+++ tests/config/test_serialization.py
@@ -1,19 +1,20 @@
+import sys
+
 import dataprep.api.corpus as api
 
 from langmodels.lmconfig.datamodel import LMTrainingConfig, LstmArch, TrainingProcedure, \
-    PrepFunction, Corpus, CosineLRSchedule
+    PrepFunction, Corpus, CosineLRSchedule, PrepFunctionOptions
 from langmodels.lmconfig.serialization import dump_config, load_config_from_string
 
 
 def test_serialize_deserialize_defaults():
     lm_training_config = LMTrainingConfig(
         base_model=None,
-        corpus=Corpus(path='/path/to/corpus',
-                        extensions="java"),
+        corpus=Corpus(path='/path/to/corpus', extensions="java"),
         prep_function=PrepFunction(
             api.bpe,
             ['10k'],
-            {'no_str': True, 'no_com': True, 'no_spaces': False, 'no_unicode': False}
+            PrepFunctionOptions(no_str=True, no_com=True, no_spaces=False, no_unicode=False, max_str_length=sys.maxsize)
         ),
         arch=LstmArch(), bs=32, bptt=200,
         training_procedure=TrainingProcedure(schedule=CosineLRSchedule(cyc_len=3, max_epochs=30, max_lr=1e-4)))

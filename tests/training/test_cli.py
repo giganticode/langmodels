1fc32859dbbb94ede04a5ec9bc9e465978f54225
hlib <hlibbabii@gmail.com>
2020-01-15 09:52:48 +0100

    fix unit-tests for save-every-epoch-feature

--- tests/training/test_cli.py
+++ tests/training/test_cli.py
@@ -14,22 +14,22 @@ def train_func_mocker(mocker: MockFixture):
 def test_defaults(train_func_mocker):
     argv = ['train']
     cli.run(argv)
-    cli.train.assert_called_with(tune=False, comet=True,
+    cli.train.assert_called_with(tune=False, comet=True, save_every_epoch=False,
                                  device_options=DeviceOptions(fallback_to_cpu=False, non_default_device_to_use=0),
                                  training_config=LMTrainingConfig())
 
 
 def test_device_comet_cpu(train_func_mocker):
-    argv = ['train', '--fallback-to-cpu', '--tune', '--disable-comet', '--device=3']
+    argv = ['train', '--fallback-to-cpu', '--tune', '--disable-comet', '--device=3', '--save-every-epoch']
     cli.run(argv)
-    cli.train.assert_called_with(tune=True, comet=False,
+    cli.train.assert_called_with(tune=True, comet=False, save_every_epoch=True,
                                  device_options=DeviceOptions(fallback_to_cpu=True, non_default_device_to_use=3),
                                  training_config=LMTrainingConfig())
 
 
 def test_short_options(train_func_mocker):
-    argv = ['train', '-Cxt', '-d 3']
+    argv = ['train', '-Cxte', '-d 3']
     cli.run(argv)
-    cli.train.assert_called_with(tune=True, comet=False,
+    cli.train.assert_called_with(tune=True, comet=False, save_every_epoch=True,
                                  device_options=DeviceOptions(fallback_to_cpu=True, non_default_device_to_use=3),
                                  training_config=LMTrainingConfig())

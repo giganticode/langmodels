ee01ac06e4455320ff34ae49ca1fe5c5658f90eb
hlib <hlibbabii@gmail.com>
2020-03-12 12:51:44 +0100

    plot multiple graphs on one plot; plotting saved data

--- langmodels/extensions/context_study.py
+++ langmodels/extensions/context_study.py
@@ -1,4 +1,5 @@
 import os
+
 import time
 from pathlib import Path
 
@@ -6,35 +7,42 @@ import jsons
 import matplotlib
 from matplotlib import pyplot as plt
 
-from typing import List
+from typing import List, Tuple
 
 from langmodels import repository
 from langmodels.evaluation import evaluate_model_on_path
 from langmodels.evaluation.customization import each_token_type_separately
 from langmodels import project_dir
 from langmodels.model.context import ContextModification
+from langmodels.util.misc import HOME
 
 
 def get_run_name(path_basename: str) -> str:
     return path_basename + "_" + "_".join(time.ctime().split(" "))
 
 
-def plot(entropies: List[float], dir: str, title: str):
+def plot(entropies_list: List[Tuple[List[float], str]], dir: str, title: str):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
     plt.xlabel("Context")
-    plt.ylabel("Entropy")
+    plt.ylabel("Increase in loss (times)")
     plt.title(title)
-    plt.plot(range(len(entropies)), entropies)
+    for entropies, token_type in entropies_list:
+        entropies = entropies[5:]
+        ax.plot(range(5, len(entropies)+5), entropies, label=token_type)
 
+    ax.legend(loc='upper right')
+    # ax.set_yscale('log')
     plt.savefig(os.path.join(dir, title))
     plt.close()
 
 
-if __name__ == '__main__':
+def run_and_plot():
     m = repository.load_default_model()
 
-    path = "/path/to/dataset"
+    path = os.path.join(HOME, "dev/raw_datasets/allamanis/java-small-test")
     result = evaluate_model_on_path(m, Path(path),
-                                    context_modification=ContextModification(max_context_length=200),
+                                    context_modification=ContextModification(max_context_length=300),
                                     token_categories=each_token_type_separately())
 
     matplotlib.use('Agg')
@@ -46,6 +54,37 @@ if __name__ == '__main__':
 
     for scenario, summary in result.items():
         title = scenario.token_category.short_summary
-        plot(list(map(lambda x: x[0], summary.values_for_contexts)), dir, title)
+        plot([(list(map(lambda x: x[0], summary.values_for_contexts)), title)], dir, title)
         with open(os.path.join(dir, f'{title}.json'), 'w') as f:
             f.write(jsons.dumps(summary))
+
+
+def relative_entropies(entropies: List[float]):
+    smallest = min(entropies)
+    return [(x - smallest) / smallest for x in entropies]
+
+
+def plot_saved_ones(path: str, filenames: List[str]):
+    matplotlib.use('Agg')
+    results_for_each_token_type = []
+    for filename in filenames:
+        with open(os.path.join(path, filename), 'r') as f:
+            obj = jsons.loads(f.read())
+            entropies = list(map(lambda x: x[0], obj['of_context_length']))
+            entropies = relative_entropies(entropies)
+            results_for_each_token_type.append((entropies, os.path.splitext(filename)[0]))
+    plot(results_for_each_token_type, path, 'MultipleTypes')
+
+
+if __name__ == '__main__':
+    # run_and_plot()
+
+    plot_saved_ones(os.path.join(project_dir, 'langmodels/figures/test_Wed_Mar__4_00_06_41_2020'), [
+        # 'ClosingBracket.json',
+        # 'Semicolon.json',
+        # 'ClosingCurlyBracket.json',
+        # 'Comment.json',
+        'KeyWord.json',
+        'splitContainer.json',
+        'ParsedToken.json',
+    ])

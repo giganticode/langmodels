680322885413d029472c7a762dcc1b6969dddb34
hlibbabii <hlibbabii@gmail.com>
2020-06-08 01:32:00 +0300

    move __main__ from training to langmodels root

--- setup.py
+++ setup.py
@@ -45,5 +45,10 @@ setup(name='giganticode-langmodels',
       keywords='big large data source code corpus machine learning nlp '
                'pytorch torch fastai language modeling',
       install_requires=requirements,
+      entry_points={
+          'console_scripts': [
+              f'langmodels = {root_package_name}.__main__:main'
+          ]
+      },
       include_package_data=True,
       zip_safe=False)

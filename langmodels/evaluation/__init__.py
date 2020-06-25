331d101a16567a21f2ee86bec03e3a385f564f0e
hlib <hlibbabii@gmail.com>
2020-06-25 11:57:50 +0200

    add 'Project', 'LinePosition', 'TokenPosition' to __all__

--- langmodels/evaluation/__init__.py
+++ langmodels/evaluation/__init__.py
@@ -1,5 +1,5 @@
 from langmodels.evaluation.api import evaluate_on_file, evaluate_on_string, evaluate_on_path
-from langmodels.evaluation.characteristics import SubtokenNumber, TokenType, FrequencyRank
+from langmodels.evaluation.characteristics import SubtokenNumber, TokenType, FrequencyRank, Project, LinePosition, TokenPosition
 from langmodels.evaluation.options import EvaluationOptions
 from langmodels.evaluation.result import EvaluationResult
 
@@ -12,7 +12,10 @@ __all__ = [
     'EvaluationOptions',
     'SubtokenNumber',
     'TokenType',
-    'FrequencyRank'
+    'FrequencyRank',
+    'Project',
+    'LinePosition',
+    'TokenPosition'
 ]
 
 

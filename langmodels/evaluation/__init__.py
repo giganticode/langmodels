e6810c3d50fe778d7f9de0256e815d36cea28d4c
hlib <hlibbabii@gmail.com>
2020-07-05 19:49:05 +0200

    do not truncate pandas dataframe when running tests on travis

--- langmodels/evaluation/__init__.py
+++ langmodels/evaluation/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from langmodels.evaluation.api import evaluate_on_file, evaluate_on_string, evaluate_on_path
 from langmodels.evaluation.characteristics import SubtokenNumber, TokenType, FrequencyRank, Project, LinePosition, TokenPosition
 from langmodels.evaluation.options import EvaluationOptions
@@ -17,5 +19,16 @@ __all__ = [
     'LinePosition',
     'TokenPosition'
 ]
+
+
+if 'LANGMODELS_EVALUATION_TEST' in os.environ:
+    from logging import getLogger
+    logger = getLogger(__name__)
+    logger.info("Running langmodels evaluation in test mode.")
+    import pandas as pd
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', -1)
 
 
